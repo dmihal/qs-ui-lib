b161e656a5c582553ae7b3a188c12816f27fd9ce
Jeff Ward <jsward.17@gmail.com>
2018-04-26 17:15:12 -0400

    convert to space

--- components/Button.js
+++ components/Button.js
@@ -6,58 +6,58 @@ import PropTypes from 'prop-types';
  * It is a "stateless" component that has no methods.
  */
 const Button = ({ children, color, size }) => (
-	<button className="button modifier">
-		{children}
-		<style jsx>{`
+  <button className="button modifier">
+    {children}
+    <style jsx>{`
       .button {
-				padding: .5em 1.5em;
-				color: ${color};
-				background-color: #fff;
-				border: 1px solid currentColor;
-				border-radius: .3em;
-				text-align: center;
-				vertical-align: middle;
-				cursor: pointer;
-				font-size: ${Button.sizes[size]};
+        padding: .5em 1.5em;
+        color: ${color};
+        background-color: #fff;
+        border: 1px solid currentColor;
+        border-radius: .3em;
+        text-align: center;
+        vertical-align: middle;
+        cursor: pointer;
+        font-size: ${Button.sizes[size]};
 
-				@media (width >= 320px) {
-	        /*background: blue;*/
-      	}	
+        @media (width >= 320px) {
+          /*background: blue;*/
+        }	
 
-				&.modifier {
-					@media (width >= 320px) {
-		        /*width: 800px;*/
-	      	}	
-				}
-			}
+        &.modifier {
+          @media (width >= 320px) {
+            /*width: 800px;*/
+          }	
+        }
+      }
     `}</style>
-	</button>
+  </button>
 )
 
 Button.propTypes = {
-	/**
-	 * Button text
-	 */
-	children: PropTypes.string.isRequired,
-	/**
-	 * Button color
-	 */
-	color: PropTypes.string,
-	/**
-	 * Button text size
-	 */
-	size: PropTypes.oneOf(['small', 'normal', 'large'])
+  /**
+   * Button text
+   */
+  children: PropTypes.string.isRequired,
+  /**
+   * Button color
+   */
+  color: PropTypes.string,
+  /**
+   * Button text size
+   */
+  size: PropTypes.oneOf(['small', 'normal', 'large'])
 }
 
 Button.defaultProps = {
-	color: '#333',
-	size: 'normal'
+  color: '#333',
+  size: 'normal'
 }
 
 Button.sizes = {
-	small: '10px',
-	normal: '20px',
-	large: '30px'
+  small: '10px',
+  normal: '20px',
+  large: '30px'
 }
 
 export default Button
--- components/RandomButton.js
+++ components/RandomButton.js
@@ -8,47 +8,47 @@ import sample from 'lodash/sample'; // randomization function
  * The button will change it's text on every click.
  */
 export default class RandomButton extends Component {
-	static propTypes = {
-		/**
-		 * List of possible texts.
-		 */
-		variants: PropTypes.array.isRequired,
-	};
+  static propTypes = {
+    /**
+     * List of possible texts.
+     */
+    variants: PropTypes.array.isRequired,
+  };
 
-	constructor(props) {
-		super();
-		this.state = {
-			text: sample(props.variants),
-		};
-	}
+  constructor(props) {
+    super();
+    this.state = {
+      text: sample(props.variants),
+    };
+  }
 
-	handleClick() {
-		this.setState({
-			text: sample(this.props.variants),
-		});
-	}
+  handleClick() {
+    this.setState({
+      text: sample(this.props.variants),
+    });
+  }
 
-	render() {
-		return (
-			<button 
-				className="random-button"
-				onClick={this.handleClick.bind(this)}>
-				{this.state.text}
-				<style jsx>{`
-      		.random-button {
-						padding: .5em 1.5em;
-						color: #666;
-						background-color: #fff;
-						border: 1px solid currentColor;
-						border-radius: .3em;
-						font-size: 16px;
-						font-weight: bold;
-						text-align: center;
-						vertical-align: middle;
-						cursor: pointer;
-					}
-      	`}</style>
-			</button>
-		);
-	}
+  render() {
+    return (
+      <button 
+        className="random-button"
+        onClick={this.handleClick.bind(this)}>
+        {this.state.text}
+        <style jsx>{`
+          .random-button {
+            padding: .5em 1.5em;
+            color: #666;
+            background-color: #fff;
+            border: 1px solid currentColor;
+            border-radius: .3em;
+            font-size: 16px;
+            font-weight: bold;
+            text-align: center;
+            vertical-align: middle;
+            cursor: pointer;
+          }
+        `}</style>
+      </button>
+    );
+  }
 }
