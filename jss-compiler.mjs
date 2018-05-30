5ec971e694b77fc6552e198337da429602906af8
Jeff Ward <jeffscottward@users.noreply.github.com>
2018-05-29 20:07:06 -0400

    Update jss-compiler.mjs
    dump gulp comments

--- jss-compiler.mjs
+++ jss-compiler.mjs
@@ -1,7 +1,3 @@
-// var gulp = require('gulp');
-// var babel = require('gulp-babel');
-// var jss = require('gulp-jss');
-
 import shell from 'shelljs';
 import fs from 'fs';
 import * as jssCli from 'jss-cli'
