7fb1f0db42def453a07f1eab2bd6a88ad80b72fd
Jeff Ward <jsward.17@gmail.com>
2018-06-06 15:01:25 -0400

    dump old global stuff

--- globals/reset-base.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'fs'
-
-import { LayoutSizes, Typography, Colors, MarginPaddingSize } from '../vars.mjs';
-const sanitize = fs.readFileSync('node_modules/sanitize.css/sanitize.css', 'utf8')
-
-export const GlobalResetBase = `
-  ${ sanitize }
-`
