11990a1f3f3651d25e2e38b9c402d3c1d10d5a36
Jeff Ward <jsward.17@gmail.com>
2018-06-06 15:06:22 -0400

    dump globals from config

--- styleguide.config.js
+++ styleguide.config.js
@@ -3,24 +3,5 @@ const globalTypeBase = require('./globals-jss-compiled/type-base-css2json.json')
 module.exports = {
 	title: 'Style Guide',
 	components: 'components/**/*.js',
-	theme: {
-		maxWidth: '1600px'
-	},
-	styles: {
-    StyleGuide: globalTypeBase
-	},
-	template: {
-		head: {
-      links: [
-				{
-					rel: 'stylesheet',
-					href: 'node_modules/sanitize.css/sanitize.css'
-				},
-				{
-					rel: 'stylesheet',
-					href: '/assets/fonts/gtwalsheim/stylesheet.css'
-				}
-			]
-    }
-  }
+	theme: { maxWidth: '1600px' }
 };
