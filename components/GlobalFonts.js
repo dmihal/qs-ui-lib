eb2aa536c380ee762c0f74391866df41a8713f30
Jeff Ward <jsward.17@gmail.com>
2018-06-19 19:30:26 -0400

    sourcing against variable not direct inject

--- components/GlobalFonts.js
+++ components/GlobalFonts.js
@@ -3,14 +3,15 @@ import PropTypes from 'prop-types';
 // import GlobalFontsBase from '../globals/fonts-base.js';
 // NOTE::::: Using Generated FontFace INSTEAD
 
+
+const fontFaceFile = require('../assets/fonts/gtwalsheim/stylesheet.css')
+
 /**
  * This is a global style baseline that is meant to be imported
  * at the top layer of your app
  */
 const GlobalFonts = () => (
-  <style jsx global>{`
-    ${ require('../assets/fonts/gtwalsheim/stylesheet.css') }
-  `}</style>
+  <style jsx global>{fontFaceFile}</style>
 )
 
 GlobalFonts.propTypes = {}
