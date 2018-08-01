39a2184abc39a9265b8117cee54ca86f3b482ad9
Jeff Ward <jsward.17@gmail.com>
2018-08-01 17:27:39 -0400

    fix(npm): main index.js

---
+++ index.js
@@ -0,0 +1,75 @@
+import Accordian from './components/Accordian.js'
+import Banner from './components/Banner.js'
+import Button from './components/Button.js'
+import CalloutCard from './components/CalloutCard.js'
+import CarouselGallery from './components/CarouselGallery.js'
+import CarouselGalleryCard from './components/CarouselGalleryCard.js'
+import CheckboxGroup from './components/CheckboxGroup.js'
+import CodeInputArea from './components/CodeInputArea.js'
+import Colors from './components/Colors.js'
+import CTAGrid from './components/CTAGrid.js'
+import CTAGridContent from './components/CTAGridContent.js'
+import DatePick from './components/DatePick.js'
+import GlobalDefault from './components/GlobalDefault.js'
+import GlobalFonts from './components/GlobalFonts.js'
+import GlobalReset from './components/GlobalReset.js'
+import GlobalTypography from './components/GlobalTypography.js'
+import Glyph from './components/Glyph.js'
+import Icon from './components/Icon.js'
+import Input from './components/Input.js'
+import LayoutGrid from './components/LayoutGrid.js'
+import LayoutWrapper from './components/LayoutWrapper.js'
+import LinkCTA from './components/LinkCTA.js'
+import LinkText from './components/LinkText.js'
+import Logo from './components/Logo.js'
+import Modal from './components/Modal.js'
+import ModalTrigger from './components/ModalTrigger.js'
+import NavigationSub from './components/NavigationSub.js'
+import NavigationTop from './components/NavigationTop.js'
+import Notification from './components/Notification.js'
+import PatternBorder from './components/PatternBorder.js'
+import RadioGroup from './components/RadioGroup.js'
+import SelectDropDown from './components/SelectDropDown.js'
+import SlideBar from './components/SlideBar.js'
+import Table from './components/Table.js'
+import Textarea from './components/Textarea.js'
+import Toggle from './components/Toggle.js'
+
+export default {
+  Accordian,
+  Banner,
+  Button,
+  CalloutCard,
+  CarouselGallery,
+  CarouselGalleryCard,
+  CheckboxGroup,
+  CodeInputArea,
+  Colors,
+  CTAGrid,
+  CTAGridContent,
+  DatePick,
+  GlobalDefault,
+  GlobalFonts,
+  GlobalReset,
+  GlobalTypography,
+  Glyph,
+  Icon,
+  Input,
+  LayoutGrid,
+  LayoutWrapper,
+  LinkCTA,
+  LinkText,
+  Logo,
+  Modal,
+  ModalTrigger,
+  NavigationSub,
+  NavigationTop,
+  Notification,
+  PatternBorder,
+  RadioGroup,
+  SelectDropDown,
+  SlideBar,
+  Table,
+  Textarea,
+  Toggle
+}
