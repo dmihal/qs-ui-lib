609fe27a458536ca3d4b46f83e7b4fb971ba3ffb
Jeff Ward <jsward.17@gmail.com>
2018-08-20 11:57:51 -0400

    cjs refrences

--- index.js
+++ index.js
@@ -1,75 +1,75 @@
-import Accordian from './dist/Accordian.js'
-import Banner from './dist/Banner.js'
-import Button from './dist/Button.js'
-import CalloutCard from './dist/CalloutCard.js'
-import CarouselGallery from './dist/CarouselGallery.js'
-import CarouselGalleryCard from './dist/CarouselGalleryCard.js'
-import CheckboxGroup from './dist/CheckboxGroup.js'
-import CodeInputArea from './dist/CodeInputArea.js'
-import Colors from './dist/Colors.js'
-import CTAGrid from './dist/CTAGrid.js'
-import CTAGridContent from './dist/CTAGridContent.js'
-import DatePick from './dist/DatePick.js'
-import GlobalDefault from './dist/GlobalDefault.js'
-import GlobalFonts from './dist/GlobalFonts.js'
-import GlobalReset from './dist/GlobalReset.js'
-import GlobalTypography from './dist/GlobalTypography.js'
-import Glyph from './dist/Glyph.js'
-import Icon from './dist/Icon.js'
-import Input from './dist/Input.js'
-import LayoutGrid from './dist/LayoutGrid.js'
-import LayoutWrapper from './dist/LayoutWrapper.js'
-import LinkCTA from './dist/LinkCTA.js'
-import LinkText from './dist/LinkText.js'
-import Logo from './dist/Logo.js'
-import Modal from './dist/Modal.js'
-import ModalTrigger from './dist/ModalTrigger.js'
-import NavigationSub from './dist/NavigationSub.js'
-import NavigationTop from './dist/NavigationTop.js'
-import Notification from './dist/Notification.js'
-import PatternBorder from './dist/PatternBorder.js'
-import RadioGroup from './dist/RadioGroup.js'
-import SelectDropDown from './dist/SelectDropDown.js'
-import SlideBar from './dist/SlideBar.js'
-import Table from './dist/Table.js'
-import Textarea from './dist/Textarea.js'
-import Toggle from './dist/Toggle.js'
+const Accordian = require('./dist/Accordian.js')
+const Banner = require('./dist/Banner.js')
+const Button = require('./dist/Button.js')
+const CalloutCard = require('./dist/CalloutCard.js')
+const CarouselGallery = require('./dist/CarouselGallery.js')
+const CarouselGalleryCard = require('./dist/CarouselGalleryCard.js')
+const CheckboxGroup = require('./dist/CheckboxGroup.js')
+const CodeInputArea = require('./dist/CodeInputArea.js')
+const Colors = require('./dist/Colors.js')
+const CTAGrid = require('./dist/CTAGrid.js')
+const CTAGridContent = require('./dist/CTAGridContent.js')
+const DatePick = require('./dist/DatePick.js')
+const GlobalDefault = require('./dist/GlobalDefault.js')
+const GlobalFonts = require('./dist/GlobalFonts.js')
+const GlobalReset = require('./dist/GlobalReset.js')
+const GlobalTypography = require('./dist/GlobalTypography.js')
+const Glyph = require('./dist/Glyph.js')
+const Icon = require('./dist/Icon.js')
+const Input = require('./dist/Input.js')
+const LayoutGrid = require('./dist/LayoutGrid.js')
+const LayoutWrapper = require('./dist/LayoutWrapper.js')
+const LinkCTA = require('./dist/LinkCTA.js')
+const LinkText = require('./dist/LinkText.js')
+const Logo = require('./dist/Logo.js')
+const Modal = require('./dist/Modal.js')
+const ModalTrigger = require('./dist/ModalTrigger.js')
+const NavigationSub = require('./dist/NavigationSub.js')
+const NavigationTop = require('./dist/NavigationTop.js')
+const Notification = require('./dist/Notification.js')
+const PatternBorder = require('./dist/PatternBorder.js')
+const RadioGroup = require('./dist/RadioGroup.js')
+const SelectDropDown = require('./dist/SelectDropDown.js')
+const SlideBar = require('./dist/SlideBar.js')
+const Table = require('./dist/Table.js')
+const Textarea = require('./dist/Textarea.js')
+const Toggle = require('./dist/Toggle.js')
 
-export default {
-  Accordian,
-  Banner,
-  Button,
-  CalloutCard,
-  CarouselGallery,
-  CarouselGalleryCard,
-  CheckboxGroup,
-  CodeInputArea,
-  Colors,
-  CTAGrid,
-  CTAGridContent,
-  DatePick,
-  GlobalDefault,
-  GlobalFonts,
-  GlobalReset,
-  GlobalTypography,
-  Glyph,
-  Icon,
-  Input,
-  LayoutGrid,
-  LayoutWrapper,
-  LinkCTA,
-  LinkText,
-  Logo,
-  Modal,
-  ModalTrigger,
-  NavigationSub,
-  NavigationTop,
-  Notification,
-  PatternBorder,
-  RadioGroup,
-  SelectDropDown,
-  SlideBar,
-  Table,
-  Textarea,
-  Toggle
+module.exports = {
+  Accordian: Accordian,
+  Banner: Banner,
+  Button: Button,
+  CalloutCard: CalloutCard,
+  CarouselGallery: CarouselGallery,
+  CarouselGalleryCard: CarouselGalleryCard,
+  CheckboxGroup: CheckboxGroup,
+  CodeInputArea: CodeInputArea,
+  Colors: Colors,
+  CTAGrid: CTAGrid,
+  CTAGridContent: CTAGridContent,
+  DatePick: DatePick,
+  GlobalDefault: GlobalDefault,
+  GlobalFonts: GlobalFonts,
+  GlobalReset: GlobalReset,
+  GlobalTypography: GlobalTypography,
+  Glyph: Glyph,
+  Icon: Icon,
+  Input: Input,
+  LayoutGrid: LayoutGrid,
+  LayoutWrapper: LayoutWrapper,
+  LinkCTA: LinkCTA,
+  LinkText: LinkText,
+  Logo: Logo,
+  Modal: Modal,
+  ModalTrigger: ModalTrigger,
+  NavigationSub: NavigationSub,
+  NavigationTop: NavigationTop,
+  Notification: Notification,
+  PatternBorder: PatternBorder,
+  RadioGroup: RadioGroup,
+  SelectDropDown: SelectDropDown,
+  SlideBar: SlideBar,
+  Table: Table,
+  Textarea: Textarea,
+  Toggle: Toggle
 }
