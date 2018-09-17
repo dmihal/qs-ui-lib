b5f42301df731f5b1f9f101967b3b489222ce94d
Jeff Ward <jsward.17@gmail.com>
2018-09-17 17:36:04 -0400

    modal trigger button style dump - agnostic

--- src/components/ModalTrigger/ModalTrigger.js
+++ src/components/ModalTrigger/ModalTrigger.js
@@ -1,34 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Typography, Colors, MarginPaddingSize } from '../vars'
 
-const ModalTrigger = ({ children, modalID }) => (
-  <label htmlFor={'modal-trigger-' + modalID} className="toggleModal">
-    {children}
+const ModalTrigger = ({ className, children, modalID }) => (
+  <label
+    htmlFor={'modal-trigger-' + modalID}
+    className={"toggleModal " + className}>
+      {children}
     <style jsx>{`
-      .toggleModal {
-        cursor: pointer;
-        text-align: center;
-        border-width: 1px;
-        border-style: solid;
-        color: ${Colors.Royal};
-
-        padding: 6px calc(${MarginPaddingSize[2]}/2);
-        font-size: ${Typography.fontSize[3]};
-
-        background: ${Colors.Royal};
-        border-color: ${Colors.Royal};
-        color: ${Colors.White};
-
-        &:hover {
-          background: ${Colors.PoloBlue};
-          border-color: ${Colors.PoloBlue};
-          color: ${Colors.White};
-        }
-        &:focus{
-          outline: none;
-        }
-      } 
+      label { cursor: pointer; }  
     `}</style>
   </label>
 )
